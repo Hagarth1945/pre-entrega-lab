df13521fab7e07bb9d54bd43cb613e6daec1115a
Santiago Campellone <sancampellone@gmail.com>
2025-10-14 21:09:34 -0300

    GET products

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hola");
+const [, , method, resource] = process.argv;
+
+method = method.toUpperCase();
+resource = resource.toLowerCase();
+
+if (method === 'GET' && resource === 'products') {
+    fetch('https://fakestoreapi.com/products')
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
