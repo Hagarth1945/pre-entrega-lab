172a00e7188f8ed4432d543f59d1e18f964035f9
Santiago Campellone <sancampellone@gmail.com>
2025-10-24 00:08:43 -0300

    GET products

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const [, , method, resource] = process.argv;
+let [, , method, resource] = process.argv;
 
 method = method.toUpperCase();
 resource = resource.toLowerCase();
+
+if(method === 'GET' && resource.startsWith("products/")) {
+    let id = resource.split("/")[1];
+    id = parseInt(id);
+    
+    if (isNaN(id) || id <= 0) {
+        console.log("ID inválido");
+    }
+    fetch(`https://fakestoreapi.com/products/` + id)
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
 
 if (method === 'GET' && resource === 'products') {
     fetch('https://fakestoreapi.com/products')
